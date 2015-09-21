0d3f1eac76bd2f7b774c04f4971b99fc450873ff
Daniel Gilbert <gilbert@ascora.de>
2015-09-21 11:02:47 +0200

    Adds SAFSensors

--- PersonalAssistantShared/src/main/java/eu/alfred/api/sensors/SAFFacade.java
+++ PersonalAssistantShared/src/main/java/eu/alfred/api/sensors/SAFFacade.java
@@ -1,7 +1,16 @@
 package eu.alfred.api.sensors;
 
+import android.os.Messenger;
+
 /**
  * Created by gilbe on 21.09.2015.
  */
 public class SAFFacade {
+    private Messenger messenger;
+
+    public SAFFacade(Messenger messenger){
+        this.messenger = messenger;
+    }
+
+    public
 }
---
+++ PersonalAssistantShared/src/main/java/eu/alfred/api/sensors/SensorListener.java
@@ -0,0 +1,7 @@
+package eu.alfred.api.sensors;
+
+/**
+ * Created by gilbe on 21.09.2015.
+ */
+public class SensorListener {
+}
