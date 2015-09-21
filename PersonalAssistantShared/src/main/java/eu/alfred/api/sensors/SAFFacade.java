a842993c0a3a046f2a8ac772d9bc8cc61e9dd8f3
Daniel Gilbert <gilbert@ascora.de>
2015-09-21 13:30:23 +0200

    Adds SAFFacade

--- PersonalAssistantShared/src/main/java/eu/alfred/api/sensors/SAFFacade.java
+++ PersonalAssistantShared/src/main/java/eu/alfred/api/sensors/SAFFacade.java
@@ -1,10 +1,18 @@
 package eu.alfred.api.sensors;
 
+import android.os.Handler;
+import android.os.Message;
 import android.os.Messenger;
+import android.os.RemoteException;
 
 import org.alfred.saf.SAFException;
 import org.alfred.saf.SensorDriver;
 import org.alfred.saf.SensorListener;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import eu.alfred.api.storage.StorageConstants;
+import eu.alfred.api.storage.responses.BucketResponse;
 
 /**
  * Created by gilbe on 21.09.2015.
@@ -12,13 +20,49 @@ import org.alfred.saf.SensorListener;
 public class SAFFacade {
     private Messenger messenger;
     private org.alfred.saf.SAFFacade safFacade;
+    private SAFFacadeReceivedResponse safFacadeReceivedResponse;
+
+    private class SAFFacadeReceivedResponse extends Handler {
+
+        public SAFFacadeReceivedResponse(){}
+
+        @Override
+        public void handleMessage(Message msg){
+            int respCode = msg.what;
+
+            switch (respCode) {
+                //Client asked for a list of contacts. Service delivers them with this response Id
+                case SAFFacadeConstants.SAF_INSTANCE_RESPONSE: {
+                    JSONObject jsonResponse = null;
+
+                    try {
+                        jsonResponse = new JSONObject(msg.getData().getString("JsonData", "{}"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    //bucketResponse.OnSuccess(jsonResponse);
+                    break;
+                }
+            }
+        }
+    }
 
     public SAFFacade(Messenger messenger) throws SAFException, IllegalArgumentException {
         this.messenger = messenger;
 
         if (messenger == null) throw new IllegalArgumentException("messenger should not be null!");
 
-        
+        safFacadeReceivedResponse = new SAFFacadeReceivedResponse();
+
+        Message msg = Message.obtain(null, SAFFacadeConstants.SAF_INSTANCE_CREATE);
+        msg.replyTo = new Messenger(safFacadeReceivedResponse);
+
+        try {
+            messenger.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     public void registerDriver(SensorDriver driver) throws SAFException {
