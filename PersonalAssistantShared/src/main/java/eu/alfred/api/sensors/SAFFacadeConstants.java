b579bbc01bc54ac574b052e8436ae6ad922cf86d
Daniel Gilbert <gilbert@ascora.de>
2015-09-21 15:53:25 +0200

    Fixes Ids for KIS

--- PersonalAssistantShared/src/main/java/eu/alfred/api/sensors/SAFFacadeConstants.java
+++ PersonalAssistantShared/src/main/java/eu/alfred/api/sensors/SAFFacadeConstants.java
@@ -4,6 +4,6 @@ package eu.alfred.api.sensors;
  * Created by gilbe on 21.09.2015.
  */
 public class SAFFacadeConstants {
-    public static final int SAF_INSTANCE_RESPONSE = 10001;
+    public static final int SAF_GET = 10001;
     public static final int SAF_INSTANCE_CREATE = 10002;
 }
--- PersonalAssistantShared/src/main/java/eu/alfred/api/storage/StorageConstants.java
+++ PersonalAssistantShared/src/main/java/eu/alfred/api/storage/StorageConstants.java
@@ -5,23 +5,23 @@ package eu.alfred.api.storage;
  */
 public class StorageConstants {
     //region RequestCodes
-    public static final int READ_STRUCTURED_DATA = 10001;
-    public static final int READ_STRUCTURED_DATA_RESPONSE = 10002;
-    public static final int READ_BINARY_DATA = 10003;
-    public static final int READ_BINARY_DATA_RESPONSE = 10004;
-    public static final int WRITE_STRUCTURED_DATA = 10005;
-    public static final int WRITE_STRUCTURED_DATA_RESPONSE = 10006;
-    public static final int WRITE_BINARY_DATA = 10007;
-    public static final int WRITE_BINARY_DATA_RESPONSE = 10008;
-    public static final int UPDATE_STRUCTURED_DATA = 10009;
-    public static final int UPDATE_STRUCTURED_DATA_RESPONSE = 10010;
-    public static final int UPDATE_BINARY_DATA = 10011;
-    public static final int UPDATE_BINARY_DATA_RESPONSE = 10012;
-    public static final int DELETE_STRUCTURED_DATA = 10013;
-    public static final int DELETE_STRUCTURED_DATA_RESPONSE = 10014;
-    public static final int DELETE_BINARY_DATA = 10015;
-    public static final int DELETE_BINARY_DATA_RESPONSE = 10016;
-    public static final int CREATE_BUCKET = 10017;
-    public static final int CREATE_BUCKET_RESPONSE = 10018;
+    public static final int READ_STRUCTURED_DATA = 30001;
+    public static final int READ_STRUCTURED_DATA_RESPONSE = 30002;
+    public static final int READ_BINARY_DATA = 30003;
+    public static final int READ_BINARY_DATA_RESPONSE = 30004;
+    public static final int WRITE_STRUCTURED_DATA = 30005;
+    public static final int WRITE_STRUCTURED_DATA_RESPONSE = 30006;
+    public static final int WRITE_BINARY_DATA = 30007;
+    public static final int WRITE_BINARY_DATA_RESPONSE = 30008;
+    public static final int UPDATE_STRUCTURED_DATA = 30009;
+    public static final int UPDATE_STRUCTURED_DATA_RESPONSE = 30010;
+    public static final int UPDATE_BINARY_DATA = 30011;
+    public static final int UPDATE_BINARY_DATA_RESPONSE = 30012;
+    public static final int DELETE_STRUCTURED_DATA = 30013;
+    public static final int DELETE_STRUCTURED_DATA_RESPONSE = 30014;
+    public static final int DELETE_BINARY_DATA = 30015;
+    public static final int DELETE_BINARY_DATA_RESPONSE = 30016;
+    public static final int CREATE_BUCKET = 30017;
+    public static final int CREATE_BUCKET_RESPONSE = 30018;
     //endregion
 }
