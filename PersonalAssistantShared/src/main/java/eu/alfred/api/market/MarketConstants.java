7e7c891721c6fcabbe279d4b08d9c0ef2c31a716
Xavier Febrer Jordà <xavier.febrer@worldline.com>
2016-02-11 11:32:10 +0100

    Integration of the Esteban Latre to the Daniel Gilbert project.

---
+++ PersonalAssistantShared/src/main/java/eu/alfred/api/market/MarketConstants.java
@@ -0,0 +1,16 @@
+package eu.alfred.api.market;
+
+public class MarketConstants {
+
+  public static final int GET_APP_DETAIL = 857910001;
+  public static final int GET_APP_LIST = 857910002;
+  public static final int GET_APP_RATE_LIST = 857910003;
+  public static final int GET_CATEGORY_IIST = 857910004;
+  public static final int GET_COUNTRY_LIST = 857910005;
+  public static final int GET_LANGUAGE_LIST = 857910006;
+  public static final int LOGIN = 857910007;
+  public static final int AUTO_LOGIN = 857910008;
+  public static final int SET_APP_RATE = 857910009;
+  public static final int SET_INSTALLED_APPS = 857910010;
+  public static final int SET_TOKEN = 857910011;
+}
