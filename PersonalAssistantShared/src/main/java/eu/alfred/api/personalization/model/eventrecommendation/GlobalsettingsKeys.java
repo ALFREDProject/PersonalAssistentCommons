c310398a762a51e398da90b8b0a66457c39d5e1d
hardes <hardes@ascora.de>
2016-04-29 13:45:51 +0200

    Globalsettings for eventrecommednatison

---
+++ PersonalAssistantShared/src/main/java/eu/alfred/api/personalization/model/eventrecommendation/GlobalsettingsKeys.java
@@ -0,0 +1,10 @@
+package eu.alfred.api.personalization.model.eventrecommendation;
+
+/**
+ * Created by thardes on 29/04/2016.
+ */
+public class GlobalsettingsKeys {
+    public static String userEventsAccepted = "userEventsAccepted";
+    public static String userEventsRejected = "userEventsRejected";
+
+}
